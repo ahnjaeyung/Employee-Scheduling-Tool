2bf5d29ecc5780540d724b0743ad3fbe5d8f6606
Tyler Bantley <tyler.bantley@icloud.com>
2022-02-02 10:23:14 -0600

    server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () =>
+      console.log(
+        `\nServer running on port ${PORT}. Visit http://localhost:${PORT}`
+      )
+    );
+      });
+
