3add67ad2a6cfe64194ad5a31f647df0714a88a8
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-02-04 11:10:28 -0600

    added more functionality for login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -87,7 +87,7 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/form', (req, res) => {
-  if (req.session.loggedIn) {
+  if (!req.session.loggedIn) {
     res.redirect('/');
     return;
   }
